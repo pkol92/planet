7e69d95cb60dd2789f8725669900be532dad7520
Paulina Kolasa <paulina_kolasa@wp.pl>
2022-05-24 12:08:15 +0200

    add axios, add loading component

---
+++ src/components/Loading/component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Center } from "../Styled/component";
+
+const Loading = () => {
+	return (
+		<div>
+			<Center style={{ fontSize: "1.3rem", color: "#f5f5f5" }}>
+				Loading...
+			</Center>
+		</div>
+	);
+};
+
+export default Loading;
--- src/components/NotFoundPage/component.tsx
+++ src/components/NotFoundPage/component.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Error } from "../Styled/component";
+import { Center } from "../Styled/component";
 
 const NotFoundPage = () => {
 	return (
 		<div>
-			<Error>Page not Found</Error>
-			<Error style={{ top: "50%" }}>
+			<Center>Page not Found</Center>
+			<Center style={{ top: "50%" }}>
 				<Link style={{ color: "#f5f5f5", fontSize: "1.5rem" }} to="/planet">
 					Go Home
 				</Link>
-			</Error>
+			</Center>
 		</div>
 	);
 };
